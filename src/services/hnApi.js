1f3e5e1e289634989a7c3cd424fca836fb5cc40a
Avi-000-Avi <atoppo267@gmail.com>
2020-11-18 20:34:17 +0530

    api working

--- src/services/hnApi.js
+++ src/services/hnApi.js
@@ -4,7 +4,7 @@ export const baseUrl = 'https://hacker-news.firebaseio.com/v0/';
 export const newStoriesUrl = `${baseUrl}newstories.json`;
 export const storyUrl = `${baseUrl}item/`;
 
-export const getStory = async storyId => {
+export const getStory = async (storyId) => {
     const result = await axios
     .get(`${storyUrl + storyId}.json`)
     .then(({data }) => data);
@@ -12,7 +12,8 @@ export const getStory = async storyId => {
     return result;
 }
 
-/**/
+/*Getting the ids of all newstories */
+/* Any function with an async will always return a promise */
 export const getStoryIds = async () => {
     const result = await axios.get(newStoriesUrl).then(({data}) => data);
     
