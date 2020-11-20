ae1d067391dfce5882c4545c7809329269892c45
Avi-000-Avi <atoppo267@gmail.com>
2020-11-20 23:51:41 +0530

    infinite-scroll

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,14 +1,29 @@
 import React, {useEffect, useState} from 'react';
-import {getStoryIds, getStory } from '../services/hnApi';
+import {getStoryIds } from '../services/hnApi';
 import {Story} from '../components/Story';
+import {GlobalStyle, StoriesContainerWrapper}  from '../styles/StoriesContainerStyles';
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]); 
 
   useEffect(() => { 
+    /*Resolving the promise */
+    /*Get the ids/data and set it to storyIds */
+    /*why is this inside of useEffect */
     getStoryIds().then(data => setStoryIds(data));
 }, []);
 
-  return storyIds.map(storyId => <Story storyId={storyId} />);
+/*For every story component we are going to pass a storyId prop/Mapping the storyids to all story components */
+  return (
+  <>
+  <GlobalStyle/>
+  <StoriesContainerWrapper data-test-id="stories-container">
+  <h1>Hacker News Stories</h1>
+  {storyIds.map(storyId => 
+  <Story key ={storyId} storyId={storyId} />
+  )}
+  </StoriesContainerWrapper>
+  </>
+  );
 };
   
